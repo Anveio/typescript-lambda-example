df89e00d0265cc86e483cadfb711c970323a5898
Shovon Hasan <im.shovonhasan@gmail.com>
2017-07-28 19:02:58 -0400

    Remove old unused types and use aws-lambda types instead

--- handler.ts
+++ handler.ts
@@ -1,7 +1,11 @@
-///<reference path="./index.d.ts"/>
+import { APIGatewayEvent, Context, Callback, ProxyResult } from 'aws-lambda';
 
-export const hello = (event: any, context: any, callback: Function): void => {
-  const response = {
+export const hello = (
+  event: APIGatewayEvent,
+  context: Context,
+  callback: Callback
+): void => {
+  const response: ProxyResult = {
     statusCode: 200,
     body: JSON.stringify({
       message: `Go Serverless v1.0! ${event} function executed successfully!`,
@@ -9,8 +13,5 @@ export const hello = (event: any, context: any, callback: Function): void => {
     })
   };
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  callback(undefined, response);
 };
